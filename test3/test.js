9d6cc84fc66198c64008a9d18dfed8bd407529ef
uttnaoki <uttnaoki@gmail.com>
2016-03-14 17:21:01 +0900

    我流dotbarのとっかかり

--- test3/test.js
+++ test3/test.js
@@ -1,81 +1,83 @@
-var dataset = [382, 40, 25, 31, 22, 9, 6, 11, 30, 22];
-// var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-//                           11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+//var dataset = [10, 20, 15];
 
-var w = window.innerWidth-100;
-var h = 500;
-var barPadding = 10;
+var margin = {
+  top: 30,
+  right: 40,
+  bottom: 30,
+  left: 70
+};
+var w = window.innerWidth;
+var h = 1000;
+var width = w - margin.left - margin.right;
+var height = h - margin.top - margin.bottom;
 var color = d3.scale.category10();
-var down=50;
-var margin = {top: 20, right: 10, bottom: 60, left: 10};
 
-//svg要素の生成
-var svg = d3.select("body")
-  .append("svg")
-  .attr({
-    width: w,
-    height: h
-  });
-var graph = svg.append("g")
-  .attr({
-    class:"graph",
-    width:w-margin.left-margin.right,
-    height:h-margin.top-margin.bottom,
-    transform:"translate("+margin.left+","+margin.top+")"
+var svg = d3.select("body").append("svg")
+  .attr("width", w)
+  .attr("height", h)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function circles(data, name, cx, cy, r) {
+  svg.selectAll(name)
+    .data(d3.range(data))
+    .enter()
+    .append("circle")
+    .attr("cx", function(d, i) {
+      return (i % 10 * 50) + cx;
+    })
+    .attr("cy", function(d, i) {
+      return Math.floor(i / 10) * 20 + cy;
+    })
+    .attr("r", r)
+    .attr("fill", function(d) {
+      return color(name);
+    })
+}
+//var dlen = dataset.length;
+
+d3.csv("test.csv", type, function(error, data) {
+  if (error) throw error;
+
+  //dot chart の出力
+  var array = data.map(function(d) {
+    return d.val1;
   })
+  var alen = array.length;
+  for (var i = 0; i < alen; i++) {
+    var tmp = array[i];
+    var rcount = 0;
+    var rsum = 0;
+    while (tmp > 0) {
+      var tmp2 = Math.floor(tmp / 10);
+      var r = rcount * 3 + 5;
+      rsum = rsum + r;
+      circles(tmp - tmp2 * 10, "circle" + i, 25, i * 100 + rsum * 2 + 20, r);
+      tmp = tmp2;
+      rcount++;
+    }
+  }
 
-graph.selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr({
-    id:"id_graph",
-    "x": function(d, i) {
-      return i * (w / dataset.length);
-    },
-    "y": function(d) {
-      return  d3.select(".graph").attr("height")- d;
-    },
-    "width": w / dataset.length - barPadding,
-    "height": function(d) {
+  svg.selectAll("text")
+    .data(data.map(function(d) {
+      return d.city;
+    }))
+    .enter()
+    .append("text")
+    .attr("x",-margin.left)
+    .attr("y", function(d, i) {
+      return i * 100+margin.top;
+    })
+    .text(function(d) {
       return d;
-    },
-    fill: function(d) {
-      return color(d);
-    }
-  });
+    })
 
-graph.selectAll("text")
-  .data(dataset)
-  .enter()
-  .append("text")
-  .text(function(d) {
-    return d;
-  })
-  .attr({
-    x: function(d, i) {
-      return i * (w / dataset.length);
-    },
-    y: function(d) {
-      return d3.select(".graph").attr("height") - (d + 10);
-    }
-  });
+})
 
-
-var xScale = d3.scale.linear()
-  .domain([0, dataset.length])
-  .range([0, w]);
-var yScale = d3.scale.linear()
-  .domain([0, d3.max(dataset, function(d) {
-    return d[1];
-  })])
-  .range([0, h]);
-
-svg.append("g")
-  .attr({
-    class:"axis",
-    transform:"translate(0,"+(h-30)+")"
-  })
-  .call(d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom"));
+function type(d) {
+  var dkeys = d3.map(d).keys();
+  dkeys.shift();
+  var dlen = dkeys.length;
+  for (var i = 0; i < dlen; i++) d[dkeys[i]] = +d[dkeys[i]];
+  return d;
+}
---
+++ test4/test.js
@@ -0,0 +1,141 @@
+//var dataset = [10, 20, 15];
+
+var margin = {
+  top: 30,
+  right: 40,
+  bottom: 30,
+  left: 70
+};
+var w = window.innerWidth;
+var h = 1500;
+var width = w - margin.left - margin.right;
+var height = h - margin.top - margin.bottom;
+var color = d3.scale.category10();
+
+var svg = d3.select("body").append("svg")
+  .attr("width", w)
+  .attr("height", h)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function update(data, name, cx, cy, r) {
+  var circles = svg.selectAll(name).data(d3.range(data))
+  circles.enter()
+    .append("circle")
+    // .attr("cx", function(d, i) {
+    //   return (i % 10 * 50) + cx;
+    // })
+    // .attr("cy", function(d, i) {
+    //   return Math.floor(i / 10) * 20 + cy;
+    // })
+    // .attr("r", r)
+    // .attr("fill", function(d) {
+    //   return color(name);
+    // });
+
+  circles.exit()
+    .transition()
+    .duration(300)
+    .attr("r", 0)
+    .attr("oppacity", 0)
+    .remove();
+
+  circles
+    .transition()
+    .duration(300)
+    .attr("cx", function(d, i) {
+      return (i % 10 * 50) + cx;
+    })
+    .attr("cy", function(d, i) {
+      return Math.floor(i / 10) * 20 + cy;
+    })
+    .attr("r", r)
+    .attr("fill", function(d) {
+      return color(name);
+    });
+}
+//var dlen = dataset.length;
+
+d3.csv("PopPerDoc.csv", type, function(error, data) {
+  if (error) throw error;
+  var dkeys = d3.map(data[0]).keys();
+  dkeys.shift();
+
+  //dot chart の出力
+  var array = data.map(function(d) {
+    return d[dkeys[0]];
+  })
+  var alen = array.length;
+  for (var i = 0; i < alen; i++) {
+    var tmp = array[i];
+    var rcount = 0;
+    var rsum = 0;
+    while (tmp > 0) {
+      var tmp2 = Math.floor(tmp / 10);
+      var r = rcount * 3 + 5;
+      rsum = rsum + r;
+      update(tmp - tmp2 * 10, "circle" + i, 25, i * 100 + rsum * 2 + 20, r);
+      tmp = tmp2;
+      rcount++;
+    }
+  }
+
+  svg.selectAll("#city")
+    .data(data.map(function(d) {
+      return d.city;
+    }))
+    .enter()
+    .append("text")
+    .attr("id", "city")
+    .attr("x", -margin.left)
+    .attr("y", function(d, i) {
+      return i * 100 + margin.top;
+    })
+    .text(function(d) {
+      return d;
+    })
+
+  svg.selectAll(".keys")
+    .data(dkeys)
+    .enter()
+    .append("text")
+    .attr("class", "keys")
+    .attr("id", "key:" + function(d) {
+      return d;
+    })
+    .attr("x", 500)
+    .attr("y", function(d, i) {
+      return i * 20 + margin.top;
+    })
+    .text(function(d) {
+      return d;
+    })
+    .on("click", function(d) {
+      var keyname = d;
+      var array = data.map(function(d) {
+        return d[keyname];
+      })
+      var alen = array.length;
+      for (var i = 0; i < alen; i++) {
+        var tmp = array[i];
+        var rcount = 0;
+        var rsum = 0;
+        while (tmp > 0) {
+          var tmp2 = Math.floor(tmp / 10);
+          var r = rcount * 3 + 5;
+          rsum = rsum + r;
+          update(tmp - tmp2 * 10, "circle" + i, 25, i * 100 + rsum * 2 + 20, r);
+          tmp = tmp2;
+          rcount++;
+        }
+      }
+    })
+})
+
+function type(d) {
+  var dkeys = d3.map(d).keys();
+  dkeys.shift();
+  var dlen = dkeys.length;
+  for (var i = 0; i < dlen; i++) d[dkeys[i]] = +d[dkeys[i]];
+  return d;
+}
